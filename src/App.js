a1f2442e4e971a09958358cd219f64f095c9174d
anantd <anantd@uw.edu>
2025-01-05 01:19:55 -0800

    Initial commit for new react portfolio project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header';
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+      <h1>Welcome to my Portfolio</h1>
+      <p>My name is Anant and I am a student at UW and here I'll be showcasing my coding projects!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
